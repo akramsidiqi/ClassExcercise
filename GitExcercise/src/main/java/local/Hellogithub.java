a6079335bb44e8aa018bc33a4f432d6bcc405d05
akramsidiqi <aksidiqi123@gmail.com>
2020-07-14 19:59:00 -0400

    a couple of comments

--- GitExcercise/src/main/java/local/Hellogithub.java
+++ GitExcercise/src/main/java/local/Hellogithub.java
@@ -5,6 +5,8 @@ public class Hellogithub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello");
+//comments 
+//this is a test class
 	}
 
 }
